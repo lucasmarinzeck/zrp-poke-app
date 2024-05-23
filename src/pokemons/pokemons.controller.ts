8a7c0a974df9605c62873d8166eada16fac675bb
Lucas Marinzeck <lucas.marinzeck@meta.com.br>
2024-05-23 13:34:07 -0300

    Custom error message + loggin on catch

--- src/pokemons/pokemons.controller.ts
+++ src/pokemons/pokemons.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, Query, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+  Render,
+} from '@nestjs/common';
 import { PokemonsService } from './pokemons.service';
 import { FindAllAbilitiesQueryDto } from './dtos/FindAllAbilitiesQueryDto';
 
@@ -17,7 +25,11 @@ export class PokemonsController {
 
       return { abilities: result };
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      throw new HttpException(
+        'Internal error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
@@ -28,20 +40,40 @@ export class PokemonsController {
       const ability = await this.pokemonsService.findAbility(id);
       return ability;
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      throw new HttpException(
+        'Internal error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   @Get('/api/abilities')
   async findAllAbilities(@Query() query: FindAllAbilitiesQueryDto) {
-    const limit = query.limit ? parseInt(query.limit, 10) : 367;
-    const offset = query.offset ? parseInt(query.offset, 10) : 0;
+    try {
+      const limit = query.limit ? parseInt(query.limit, 10) : 367;
+      const offset = query.offset ? parseInt(query.offset, 10) : 0;
 
-    return await this.pokemonsService.findAllAbilities(limit, offset);
+      return await this.pokemonsService.findAllAbilities(limit, offset);
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Internal error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Get('/api/ability/:id')
   async getAbility(@Param('id') id: string) {
-    return this.pokemonsService.findAbility(id);
+    try {
+      return this.pokemonsService.findAbility(id);
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Internal error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
